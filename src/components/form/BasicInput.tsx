1483c90caa979baa0c608392f629b61f337dfc59
josepplloo <josepplloo@gmail.com>
2024-07-03 16:42:37 -0400

    adding business rules for all the components, is missing the rule for calc the base price

--- src/components/form/BasicInput.tsx
+++ src/components/form/BasicInput.tsx
@@ -2,6 +2,7 @@
 import { useDispatch } from "@/contexts/QuoteContext";
 import { actionCreators } from "@/contexts/QuoteContext/reducer";
 import { FieldValueType } from "./types";
+import { useState } from "react";
 
 interface MyFieldProps {
   title: string;
@@ -15,18 +16,22 @@ const inputStyle =
 export const BasicInput = (props: MyFieldProps) => {
   const { title, type, name, id } = props;
   const isRCButton = type === 'radio' || type === 'checkbox';
+  const isCheckbox = type === 'checkbox';
+  const [checked, setChecked] = useState(false);
 
   const dispatch = useDispatch();
-  const handleChange = (value: FieldValueType) => { 
+  const handleChange = (value: FieldValueType) => {
+    // handler for the checkbox set the check in the current state and send it also to the global state
+    setChecked(!checked);
     const payload = {
       fieldID: name,
-      fieldValue: value,
+      fieldValue: isCheckbox ? !checked : value,
     };    
     dispatch(actionCreators.setForm(payload));
   };
 
   return (
-    <p className="w-full hover:bg-indigo-50 flex justify-between p-2 has-[:checked]:bg-indigo-300 has-[:checked]:rounded">
+    <p className="w-full hover:bg-indigo-50 dark:hover:bg-indigo-500 flex justify-between p-2 has-[:checked]:bg-indigo-300 has-[:checked]:rounded">
       <label htmlFor={id} className="hover:cursor-pointer w-full mb-2 text-sm font-medium text-gray-900 dark:text-white">
         <span>{title}</span>
         <strong>
@@ -41,6 +46,7 @@ export const BasicInput = (props: MyFieldProps) => {
         className={inputStyle}
         onChange={(e) => handleChange(e.target.value)}
         size={30}
+        checked={isCheckbox? checked : undefined}
       />
     </p>
   );
